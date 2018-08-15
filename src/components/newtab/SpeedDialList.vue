af585bb5e1d8c4f4d218960de5542cad58e2e553
孤月蓝风 <trlanfeng@foxmail.com>
2018-08-15 12:12:08 +0800

    快速拨号vue改版完成

--- src/components/newtab/SpeedDialList.vue
+++ src/components/newtab/SpeedDialList.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="SpeedDialContainer">
-    <draggable v-model="sddata.list">
-      <div v-for="(item,index) in sddata.list" :key="index" class="test">
-        <h2>{{item.name}}</h2>
-        <p>{{item.url}}</p>
+    <draggable v-model="sddata.list" :options="draggableOptions" class="SpeedDialBox">
+      <div v-for="(item,index) in sddata.list" :key="index" class="speeddial col-3 col-md-3 col-lg-3">
+        <a :href="item.url" class="block" :title="item.name">
+          <div class="icon">
+            <img :src="item.url | getFavIconUrl" :alt="item.name" @load="imgLoad($event)">
+            <span class="word">{{item.name | getFirstWord}}</span>
+          </div>
+          <span class="title">{{item.name}}</span>
+        </a>
+        <div :id="'delete_button_'+index" class="close_button">
+          <img src="../../assets/images/delete.png">
+        </div>
       </div>
     </draggable>
   </div>
@@ -18,19 +26,104 @@ export default Vue.extend({
   },
   data() {
     return {
-      sddata
+      sddata,
+      draggableOptions: {
+        animation: 250
+      }
     };
   },
-  mounted() {
-    console.log("123");
+  methods: {
+    imgLoad(e) {
+      e.target.style.display = "inline"
+    }
+  },
+  mounted() {},
+  filters: {
+    getFavIconUrl(url) {
+      var domain = url.split("/");
+      domain = domain[0] + "//" + domain[2];
+      return domain + "/favicon.ico";
+    },
+    getFirstWord(title: string) {
+      return title.substr(0, 1).toUpperCase();
+    }
   }
 });
 </script>
 <style lang="less" scoped>
-  #SpeedDialContainer {
-    display: flex;
-    .test {
-      width: 20%;
+.SpeedDialBox {
+  max-width: 1200px;
+  margin: 50px auto 0;
+  display: flex;
+  flex-wrap: wrap;
+  .speeddial {
+    transition: all 0.5s;
+    position: relative;
+    margin-top: 20px;
+    .block {
+      display: flex;
+      height: 52px;
+      align-items: center;
+      padding: 10px;
+      color: #333;
+      background-color: white;
+      border-radius: 5px;
+      text-decoration: none;
+      overflow: hidden;
+      &:hover,
+      &:focus {
+        text-decoration: none;
+        box-shadow: 0 0 20px 0 #333;
+        span {
+          text-decoration: none;
+        }
+      }
+      .icon {
+        width: 32px;
+        height: 32px;
+        margin-right: 10px;
+        overflow: hidden;
+        img {
+          display: none;
+          width: 32px;
+          height: 32px;
+          background-color: white;
+        }
+        .word {
+          display: inline-block;
+          font-size: 24px;
+          background-color: #0090FF;
+          color: white;
+          width: 32px;
+          height: 32px;
+          text-align: center;
+          line-height: 32px;
+          border-radius: 3px;
+        }
+      }
+      .title {
+        flex: 1;
+        white-space: nowrap;
+        font-size: 14px;
+        text-overflow: ellipsis;
+        overflow: hidden;
+      }
+    }
+    .close_button {
+      display: none;
+      width: 32px;
+      height: 32px;
+      position: absolute;
+      top: 10px;
+      right: 24px;
+      // background: linear-gradient(to left, rgba(255,255,255,0.3), transparent);
+      background-color: white;
+      cursor: pointer;
+      img {
+        width: 32px;
+        height: 32px;
+      }
     }
   }
+}
 </style>
--- src/newtab.js
+++ src/newtab.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './components/newtab/Newtab.vue';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/css/newtab.less'
 
 Vue.config.productionTip = false;
