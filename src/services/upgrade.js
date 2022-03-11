6985bd92000d919daf84cbb1ba8f657b634ac558
trlanfeng <trlanfeng@foxmail.com>
2022-03-11 14:34:07 +0800

    调整数据升级程序

--- src/services/upgrade.js
+++ src/services/upgrade.js
@@ -1,4 +1,5 @@
-export const curVersion = 3;
+// 数据结构版本号，与应用版本无关，单独计数
+export const curVersion = 2;
 
 export const defaultSettings = {
   1: {
@@ -14,7 +15,7 @@ export const defaultSettings = {
     bgLastCheckDate: 0,
     bgBlur: 20,
   },
-  3: {
+  2: {
     categories: ['default'],
     links: {
       default: {
@@ -37,14 +38,14 @@ export const defaultSettings = {
         'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US',
       type: 1,
     },
-    version: 3,
+    version: 2,
   },
 };
 
 export function upgrade(version, data) {
   if (version >= curVersion) return data;
-  if (version < 3) {
-    const newData = defaultSettings['3'];
+  if (version < 2) {
+    const newData = defaultSettings['2'];
     newData.links.default.list = data.list;
     newData.search.show = data.isSearchOpen;
     newData.search.icon = data.searchIcon;
@@ -55,7 +56,47 @@ export function upgrade(version, data) {
     newData.background.url = data.bgUrl;
     newData.background.bingApi = data.bingApiUrl;
     newData.background.type = data.bgType;
-    console.log(`从版本 ${version} 升级到 3 成功`);
-    return upgrade(3, newData);
+    console.log(`从版本 ${version} 升级到 2 成功`);
+    return upgrade(2, newData);
   }
+}
+
+function from1to2(data) {
+  const newData = defaultSettings['2'];
+  newData.links.default.list = data.list;
+  newData.search.show = data.isSearchOpen;
+  newData.search.icon = data.searchIcon;
+  newData.search.title = data.searchTitle;
+  newData.search.url = data.searchUrl;
+  newData.background.blur = data.bgBlur;
+  newData.background.lastCheckDays = data.bgLastCheckDate;
+  newData.background.url = data.bgUrl;
+  newData.background.bingApi = data.bingApiUrl;
+  newData.background.type = data.bgType;
+  console.log(`从版本 1 升级到 2 成功`);
+  return newData;
+}
+
+const upgradePrograms = {
+  from1to2,
+};
+
+export function checkNeedUpgrade(data) {
+  const v = data.version || 1;
+  if (v === curVersion) {
+    console.log('数据不需要升级');
+    return false;
+  }
+  return true;
+}
+
+export function upgradeData(data) {
+  let v = data.version || 1;
+  let finalData = {};
+  while (v < curVersion) {
+    const funcName = `from${v}to${v + 1}`;
+    finalData = upgradePrograms[funcName]();
+    v++;
+  }
+  return finalData;
 }
